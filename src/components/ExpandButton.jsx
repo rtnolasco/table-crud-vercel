e1b567fdac7afe79882e936238d902494c158754
rtnolasco <rtnolasco@hotmail.com>
2022-12-27 22:48:25 +0800

    add bottom border on button

--- src/components/ExpandButton.jsx
+++ src/components/ExpandButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import chevron from '../assets/fi_chevrons-down.svg'
 export const ExpandButton = ({isOpen, toggle}) => {
     return (
-        <td onClick={toggle} style={{borderBottom: "none"}}>
+        <td onClick={toggle}>
             <span className="img-bg"  style=
                             {{transform: `rotate(${isOpen ? 180 :0 }deg)`,
                                 transition: "all 0.25s"
