76c38fafcc6771de0b5c734bde25210ca337c78e
rtnolasco <rtnolasco@hotmail.com>
2022-12-28 18:31:12 +0800

    button style adustments

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,24 @@
-import {React, useContext} from 'react'
+import {React, useContext, useState} from 'react'
+import { Modal, Form, Button, Container, Row, Col } from 'react-bootstrap'
 import { TableRow } from './TableRow';
 import { ExpandButton } from './ExpandButton';
 import useOpenController from '../hooks/useOpenController';
+import EditForm from './modal/EditForm';
 
 import { ProductContext } from '../context/ProductContext';
 
 const Product = ({product, loading, }) => {
+
+    const [show, setShow] = useState(false)
+    const handleShowModal = () => setShow (true)
+    const handleCloseModal = () => setShow (false)
+
     const {deleteProduct} = useContext(ProductContext)
     const {isOpen, toggle}= useOpenController(false)
 
 if (loading) {
     return <td>Loading...</td>
 }
-
-
     return (
         
         <>
@@ -23,11 +28,38 @@ if (loading) {
                 <td>{product.category}</td> 
                 <td>${product.price}</td> 
                 <td><img src={product.image} className="tbl-image"></img></td> 
-                <td style={{width:"111px"}}> 
-                    <a href="#" className="edit" data-toggle="modal"> Edit </a> &nbsp;   
-                    <button onClick={() => deleteProduct(product.id)} className="btn text-danger btn-act" data-toggle="modal"> Delete </button>
+                <td style={{width:"130px"}}> 
+                    <button onClick={handleShowModal} className="btn-edit" data-toggle="modal"> Edit </button> &nbsp;   
+                    <button onClick={() => deleteProduct(product.id)} className="btn-edit" data-toggle="modal"> Delete </button>
                 </td> 
-               
+                <Modal show={show} onHide={handleCloseModal}dialogClassName="modal-90w">
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {/* <EditForm ref={addFormRef} close={() => handleCloseModal()} /> */}
+                        <EditForm />
+                    </Modal.Body>
+                    <Container style={{marginBottom:"10px"}}>
+                                <Row>
+                                    <Col>
+                                        <Button onClick={handleCloseModal} className="btn-modal-cancel">Cancel</Button>
+                                    </Col>
+                                    <Col>
+                                        
+                                        {/* <Button onClick={()=>addFormRef.current.callhandleSubmit()} className="btn-modal-add">Add</Button> */}
+                                        <Button className="btn-modal-add">Add</Button> 
+                                    
+                                    </Col>
+                                </Row>
+                        </Container>
+                    <Modal.Footer>
+                            
+                    </Modal.Footer>
+                </Modal>
+
                 <ExpandButton isOpen={isOpen} toggle={toggle}/>
                   
                
