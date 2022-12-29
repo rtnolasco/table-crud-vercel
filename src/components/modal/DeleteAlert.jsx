b190773d6439ceedfd194c3516c0145931b9ab6e
rtnolasco <rtnolasco@hotmail.com>
2022-12-29 21:45:18 +0800

    added-delete-modal-func

--- src/components/modal/DeleteAlert.jsx
+++ src/components/modal/DeleteAlert.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const DeleteAlert = ({productId, productTitle}) => {
   return (
     <div><h5>You are about to delete a product</h5>
-    <p>you are about to delete {productId}: {productTitle}?</p></div>
+    <p>Are you sure you want to delete {productId}: {productTitle}?</p></div>
   )
 }
 
