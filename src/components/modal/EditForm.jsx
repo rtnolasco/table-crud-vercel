7d02fe0ac8a895db3c32b205c1f9d9ebb187c44d
Developer <developer@Developers-MacBook-Pro.local>
2023-01-12 19:08:11 +0800

    modify edit form -category issue

--- src/components/modal/EditForm.jsx
+++ src/components/modal/EditForm.jsx
@@ -1,198 +1,211 @@
-import {React, useContext, useState,forwardRef, useImperativeHandle} from 'react'
-import { Form, Button, Container, Row, Col } from 'react-bootstrap'
-import StarRating from '../StarsRating'
-import { ProductContext } from '../../context/ProductContext'
+import { React, useContext, useState, forwardRef, useImperativeHandle } from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import StarRating from '../StarsRating';
+import { ProductContext } from '../../context/ProductContext';
 
+const EditForm = forwardRef(({ productsEdit }, ref) => {
+	const id = productsEdit.id;
+	const { updateProduct } = useContext(ProductContext);
 
-const EditForm = forwardRef(({productsEdit}, ref) => {
+	const [title, setTitle] = useState(productsEdit.title);
+	const [category, setCategory] = useState([]);
+	const [price, setPrice] = useState(productsEdit.price);
+	const [image, setImage] = useState(productsEdit.image);
+	const [description, setDescription] = useState(productsEdit.description);
 
-  const id = productsEdit.id;
-  const {updateProduct} = useContext(ProductContext)
+	const handleChangeCheckbox = (e) => {
+		// const { value, checked } = e.target;
+		console.log('value', e.target.value);
+		console.log('catgegoryf', category);
+		if (e.target.name === 'category') {
+			// let copy = { ...category };
 
-  const [title,setTitle] = useState(productsEdit.title)
-  const [category,setCategory] = useState(productsEdit.category)
-  const [price,setPrice] = useState(productsEdit.price)
-  const [image,setImage] = useState(productsEdit.image)
-  const [description,setDescription] = useState(productsEdit.description)
+			if (e.target.checked) {
+				category.push(e.target.value);
+			}
+			//  else {
+			// 		copy.category = copy.category.filter((el) => el != e.target.value);
+			// 	}
+			// 	setCategory(copy);
+			console.log('catgegor2', category);
+		} else {
+			setCategory(() => ({
+				...category,
+				[e.target.name]: e.target.value,
+			}));
+		}
+	};
 
- 
+	const updatedProduct = { id, title, category, price, image, description };
 
-  //Vlaue State
-  // const [formValue, setFormValue] = useState({
-  //   title: "",
-  //   category: "",
-  //   price: "",
-  //   image: "",
-  //   description: ""
-  // });
- 
-  const updatedProduct = {id,title,category,price,image,description}
-//<form onSubmit={(e) => handleSubmit(e, sendprop)}>
-  const handleSubmit = (e) => {
-    // e.preventDefault();
-    updateProduct(id,updatedProduct)
-    
-  }
+	const handleSubmit = (e) => {
+		// e.preventDefault();
+		updateProduct(id, updatedProduct);
+	};
 
-  useImperativeHandle(ref, () => ({
-    callhandleSubmit() {
-      handleSubmit()
-     
-    }
+	useImperativeHandle(ref, () => ({
+		callhandleSubmit() {
+			handleSubmit();
+		},
+	}));
 
-  }))
+	//  const handleOnChange = (e) = {
 
+	//  }
 
-//  const handleOnChange = (e) = {
+	// const [categoryInfo, setcategoryInfo] = useState({
+	// 	cat1: [],
+	// 	category: [],
+	// });
 
+	// const handleCheckBox = (e) => {
+	// 	// Destructuring
+	// 	const { value, checked } = e.target;
+	// 	const { cat1 } = categoryInfo;
 
-//  }
+	// 	console.log(`${value} is ${checked}`);
 
-  return (
-    <Container>
-    <Form onSubmit={handleSubmit}>
-      <Row>
-        <Col>
-          <Form.Group>
-          <Form.Label>ID</Form.Label>
-            <Form.Control
-              type = "text"
-              name="id"
-              
-              disabled
-              >
-            </Form.Control>
-          </Form.Group>
+	// 	// Case 1 : The user checks the box
+	// 	if (checked) {
+	// 		setcategoryInfo({
+	// 			cat1: [...cat1, value],
+	// 			category: [...cat1, value],
+	// 		});
+	// 		console.log('value', cat1);
+	// 	}
 
-      
-         <Form.Group>
-         <Form.Label>Title</Form.Label>
-            <Form.Control
-              type = "text"
-              name="title"
-              value={title}
-              onChange = { (e) => setTitle(e.target.value)}
-            
-              >
-            </Form.Control>
-          </Form.Group>
-        </Col>
-        
-        <Col>
-        <Form.Group>
-          <Form.Label>Image Link</Form.Label>
-        <Form.Control
-          type = "text"
-          name="image"
-          value={image}
-          onChange = { (e) => setImage(e.target.value)}
-       
-         
-          
-          >
+	// 	// Case 2  : The user unchecks the box
+	// 	else {
+	// 		setcategoryInfo({
+	// 			cat1: cat1.filter((e) => e !== value),
+	// 			category: cat1.filter((e) => e !== value),
+	// 		});
+	// 	}
+	// };
 
-        </Form.Control>
-        </Form.Group>
-         
-          <Form.Group>
-          <Form.Label>Price</Form.Label>
-            <Form.Control
-                type = "text"
-                name="price"
-                value={price}
-                onChange = { (e) => setPrice(e.target.value)}
-              
-              >
-            </Form.Control>
-          </Form.Group>  
-        </Col>
-      </Row>
-        
-        <Form.Group>
-          <Form.Label>Description</Form.Label>
-        <Form.Control
-          as = "textarea"
-          name="description"
-          value={description}
-          onChange = { (e) => setDescription(e.target.value)}
-        
-         >
-        </Form.Control>
-        </Form.Group>
-        <Form.Group>
-          
-            {/* <Form.Control
+	return (
+		<Container>
+			<Form onSubmit={handleSubmit}>
+				<Row>
+					<Col>
+						<Form.Group>
+							<Form.Label>ID</Form.Label>
+							<Form.Control type="text" name="id" disabled></Form.Control>
+						</Form.Group>
+
+						<Form.Group>
+							<Form.Label>Title</Form.Label>
+							<Form.Control
+								type="text"
+								name="title"
+								value={title}
+								onChange={(e) => setTitle(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+					</Col>
+
+					<Col>
+						<Form.Group>
+							<Form.Label>Image Link</Form.Label>
+							<Form.Control
+								type="text"
+								name="image"
+								value={image}
+								onChange={(e) => setImage(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+
+						<Form.Group>
+							<Form.Label>Price</Form.Label>
+							<Form.Control
+								type="text"
+								name="price"
+								value={price}
+								onChange={(e) => setPrice(e.target.value)}
+							></Form.Control>
+						</Form.Group>
+					</Col>
+				</Row>
+
+				<Form.Group>
+					<Form.Label>Description</Form.Label>
+					<Form.Control
+						as="textarea"
+						name="description"
+						value={description}
+						onChange={(e) => setDescription(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				<Form.Group>
+					{/* <Form.Control
               type = "hidden"
               name="category"
               value="Electronics"
            
               >
             </Form.Control> */}
-            
-          <Form.Label>Category</Form.Label>
-            {/* <Form.Control
-              type = "text"
-              name="category"
-              value = {category}
-              onChange = { (e) => setCategory(e.target.value)}
-              > */}
-                
-    
-        <div key="inline-checkbox" className="modal-category-wrap">
-          <Form.Check
-            inline
-            label="Jewelery"
-            name="group1"
-            type="checkbox"
-            id="inline-checkbox1"
-            
-          />
-          <Form.Check
-            inline
-            label="Womens Clothing"
-            name="group1"
-            type="checkbox"
-            id="inline-checkbox2"
-          />
-          <Form.Check
-            inline
-            
-            label="Mens Clothing"
-            type="checkbox"
-            id="inline-checkbox3"
-          />
-          <Form.Check
-            inline
-            
-            label="Electronics"
-            type="checkbox"
-            id="inline-checkbox3"
-          />
-        </div>
-  
-    
 
-            {/* </Form.Control> */}
-          </Form.Group> 
-        
-      
-        
-        <Form.Group>
-          <Form.Label>Rating</Form.Label>
-        
+					<Form.Label>Category</Form.Label>
+					{/* <Form.Control
+						type="text"
+						name="category"
+						value={category}
+						onChange={(e) => setCategory(e.target.value)}
+					></Form.Control> */}
 
-      
-        
-         <div style={{display:"flex"}}> <StarRating /> </div>
+					<div key="inline-checkbox" className="modal-category-wrap">
+						<Form.Check
+							inline
+							label="Jewelery"
+							name="category"
+							type="checkbox"
+							id="inline-checkbox1"
+							value="jewelery"
+							onChange={handleChangeCheckbox}
+						/>
+						<Form.Check
+							inline
+							label="Womens Clothing"
+							name="category"
+							type="checkbox"
+							id="inline-checkbox2"
+							value="women's clothing"
+							onChange={handleChangeCheckbox}
+						/>
+						<Form.Check
+							inline
+							label="Mens Clothing"
+							name="category"
+							type="checkbox"
+							id="inline-checkbox3"
+							value="men's clothing"
+							onChange={handleChangeCheckbox}
+						/>
+						<Form.Check
+							inline
+							label="Electronics"
+							name="category"
+							type="checkbox"
+							id="inline-checkbox3"
+							value="electronics"
+							onChange={handleChangeCheckbox}
+						/>
+					</div>
+				</Form.Group>
 
-        
-      </Form.Group>
-    {/* <Button variant="succes" type="submit" >Add new product</Button>  
+				<Form.Group>
+					<Form.Label>Rating</Form.Label>
+
+					<div style={{ display: 'flex' }}>
+						{' '}
+						<StarRating />{' '}
+					</div>
+				</Form.Group>
+				{/* <Button variant="succes" type="submit" >Add new product</Button>  
     <Button onClick={() => props.close()}>close</Button> */}
-      
-    </Form>
-    </Container>
-  )
-})
+			</Form>
+		</Container>
+	);
+});
 
-export default EditForm
+export default EditForm;
